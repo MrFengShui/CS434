0e9ca66badebe6eaa4fd3c646719bd3a471fdf86
MrFengShui <luansongjian@outlook.com>
2017-04-26 11:51:30 -0700

    merge

--- HW3/Code/Assignment_Three_Final.py
+++ HW3/Code/Assignment_Three_Final.py
@@ -6,15 +6,23 @@ from scipy.special import expit
 
 np.set_printoptions(precision=3,suppress=True)
 
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 ## Part 1
 class DataFormat():
 
+=======
+	
+## Part 1
+class DataFormat():
+	
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
 	def __init__(self, name):
 		self.name = name
 
 	def func_norm_data(self):
 		src_data = genfromtxt(self.name, delimiter=',')
 		true_data, feature_data = src_data[:,0], np.delete(src_data, 0, axis=1)
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 
 		feature_max, feature_min = np.amax(feature_data), np.amin(feature_data)
 		feature_range, feature_avg = feature_max - feature_min, np.average(feature_data)
@@ -23,10 +31,21 @@ class DataFormat():
 		self.norm_data, self.true_data = np.absolute(feature_norm), true_data
 		return np.absolute(feature_norm), true_data
 
+=======
+		
+		feature_max, feature_min = np.amax(feature_data), np.amin(feature_data)
+		feature_range, feature_avg = feature_max - feature_min, np.average(feature_data)
+		feature_norm = (feature_data - feature_avg) / feature_range
+		
+		self.norm_data, self.true_data = np.absolute(feature_norm), true_data
+		return np.absolute(feature_norm), true_data
+		
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
 	def func_leave_out(self, index):
 		return self.norm_data[index], self.true_data[index]
 
 class KNN():
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 
 	def __init__(self, norm_data):
 		self.norm_data = norm_data
@@ -35,6 +54,13 @@ class KNN():
         diff_sqrt = (test_data - self.norm_data) ** 2
         distance_data = np.sqrt(np.sum(diff_sqrt, axis=1))
         print '+++', distance_data
+=======
+	
+	def __init__(self, norm_data):
+		self.norm_data = norm_data
+		
+	def func_classify():
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
 # Neigbor = collections.namedtuple('Neigbor', 'distance_data true_data')
 
 # def func_data_class(norm_data, true_data, test_data, k = 1):
@@ -61,15 +87,22 @@ if __name__ == '__main__':
 	train_norm_data, true_data = train_data.func_norm_data()
 	# train_leave_out = train_data.func_leave_out(3)
 	# print train_norm_data, train_leave_out
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
     knn_train = KNN(train_norm_data)
     knn_train.func_classify(train_norm_data[0])
     
+=======
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
 	# raw_train, raw_test = func_read_data()
 	# true_data, feature_data = func_filter_data(raw_train)
 	# test_true_data, test_feature_data = func_filter_data(raw_test)
 	# norm_data, test_norm_data = func_norm_data(feature_data), func_norm_data(test_feature_data)
 	# k_list, train_error_list, cross_valid_list, test_error_list = range(1, 52, 2), [], [], []
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 
+=======
+	
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
 	# for k in k_list:
 		# train_error = func_calc_error(norm_data, true_data, k)
 		# train_error_list.append(train_error)
@@ -80,4 +113,8 @@ if __name__ == '__main__':
 
 	# print 'Training:', train_error_list
 	# print 'Cross Valid:', cross_valid_list
+<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 	# print 'Testing:', test_error_list
+=======
+	# print 'Testing:', test_error_list
+>>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
---
+++ HW3/Code/Sample_Code.py
@@ -0,0 +1,165 @@
+import math
+import numpy as np
+from collections import namedtuple
+from operator import attrgetter
+
+DistanceOutputPair = namedtuple('DistanceOutputPair', 'distance output')
+FeatureOutputPair = namedtuple('FeatureOutputPair', 'features output')
+
+class CsvPrinter:
+	def __init__(self, filename, labels=[], delimiter=','):
+		if labels:
+			self.file = open(filename, 'w')
+			self.columns = len(labels)
+			labels.append('\n')
+			self.delimiter = delimiter
+			self.file.write(self.delimiter.join(labels))
+		else:
+			print "CsvPrinter: Please provide column labels"
+			exit()
+
+	def writerow(self, data):
+		if len(data) == self.columns:
+			data.append('\n')
+			self.file.write(self.delimiter.join([str(x) for x in data]))
+		else:
+			print "CsvPrinter: Data length should match number of labels"
+			exit()
+
+	def close(self):
+		self.file.close()
+
+class Data:
+	def __init__(self, filename=None, features=None, outputs=None):
+		if filename:
+			file = open(filename)
+			(self.features, self.outputs) = self._build_data_arrays(file)
+			file.close()
+		elif features and outputs:
+			self.features = features
+			self.outputs = outputs
+		self._features = self.features
+		self._outputs = self.outputs
+
+	def normalize(self):
+		features_max = np.amax(self.features, axis=0)
+		features_min = np.amin(self.features, axis=0)
+		features_spread = features_max - features_min
+		self.features = (self.features - features_min) / features_spread
+		self._features = self.features
+
+	def leave_out(self, index):
+		self.features = np.delete(self._features, index, axis=0)
+		self.outputs = np.delete(self._outputs, index, axis=0)
+		return FeatureOutputPair(self._features[index], self._outputs[index])
+
+	def include_all(self):
+		self.features = self._features
+		self.outputs = self._outputs
+
+	def _build_data_arrays(self, file):
+		(features, outputs) = self._build_data_lists(file)
+		return (np.array(features, dtype=float), np.array(outputs, dtype=int))
+
+	def _build_data_lists(self, file):
+		(features, outputs) = ([], [])
+		for line in file:
+			(line_features, line_output) = self._extract_features_and_output(line)
+			features.append(line_features)
+			outputs.append(line_output)
+		return (features, outputs)
+
+	def _extract_features_and_output(self, line):
+		features_and_output = line.split(',')
+		return (features_and_output[1:], features_and_output[0])
+
+class Knn:
+	def __init__(self, training_data):
+		self.training_data = training_data
+
+	def get_testing_error(self, data, k=1):
+		outputs = self._test(data, k)
+		return (sum(abs(outputs - data.outputs)) / float(2 * len(data.outputs)))
+
+	def get_training_error(self, k=1):
+		outputs = self._test(self.training_data, k)
+		return (sum(abs(outputs - self.training_data.outputs)) /
+				float(2 * len(self.training_data.outputs)))
+
+	# Specifically for leave-one-out cross-validation
+	def get_cross_validation_error(self, k=1):
+		total_error = 0
+		print '+++', self.training_data.leave_out(0)
+		for i in range(len(self.training_data.features)):
+			left_out = self.training_data.leave_out(i)
+			output = self._classify(left_out.features, k)
+			total_error += abs(left_out.output - output)/2
+		self.training_data.include_all()
+		return float(total_error) / len(self.training_data.outputs)
+
+	def _test(self, data, k):
+		return map(lambda instance: self._classify(instance, k), data.features)
+
+	def _classify(self, test_instance, k):
+		# print '+++', test_instance
+		neighbors = self._get_nearest_neighbors(test_instance)
+		output = sum([neigbor.output for neigbor in neighbors[:k]])
+		if output < 0: return -1
+		else: return 1
+
+	def _get_nearest_neighbors(self, test_instance):
+		# print '+++', test_instance, self.training_data.features
+		distances = np.sqrt(np.sum(
+				(test_instance - self.training_data.features)**2,
+				axis=1
+				))
+
+		neigbors = map(DistanceOutputPair, distances,
+			self.training_data.outputs)
+
+		return sorted(neigbors, key=attrgetter('distance'))
+
+training_data = Data('knn_train.csv')
+testing_data = Data('knn_test.csv')
+training_data.normalize()
+testing_data.normalize()
+
+def main():
+	part_1()
+	part_2()
+	extra_credit()
+
+def part_1():
+	labels=['k', 'Training Error', 'Testing Error', 'Leave-one-out Error']
+	csv = CsvPrinter('part_1.csv', labels)
+	model = Knn(training_data)
+	cv_err = model.get_cross_validation_error(k=1)
+	# def _print_results():
+		# print "---------------------------"
+		# print "K-value: ", k
+		# print "".join(["Training Error: ",
+			# str(round(training_err, 4) * 100), "%"]
+			# )
+		# print "".join(["Leave-One-Out Error: ",
+			# str(round(cv_err, 4) * 100), "%"]
+			# )
+		# print "".join(["Testing Error: ",
+			# str(round(testing_err, 4) * 100), "%"]
+			# )
+
+	# for k in range(1, 52, 2):
+		# training_err = model.get_training_error(k=k)
+		# testing_err = model.get_testing_error(testing_data, k=k)
+		# cv_err = model.get_cross_validation_error(k=k)
+		# _print_results()
+		# csv.writerow([k, training_err, testing_err, cv_err])
+	print "---------------------------"
+	csv.close()
+
+def part_2():
+	return
+
+def extra_credit():
+	return
+
+if __name__ == '__main__':main()
