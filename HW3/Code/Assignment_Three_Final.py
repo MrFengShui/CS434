c23969353d0d9ddef6d023bdd115324f6bd9b790
MrFengShui <luansongjian@outlook.com>
2017-04-26 12:10:32 -0700

    fix merge error

--- HW3/Code/Assignment_Three_Final.py
+++ HW3/Code/Assignment_Three_Final.py
@@ -1,120 +1,117 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# Plotting library
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import gridspec
+
+# Numerical Library
 import numpy as np
 from numpy import genfromtxt
 import collections, math, operator
 
 from scipy.special import expit
 
+# Limit printout to 3 decimal places
 np.set_printoptions(precision=3,suppress=True)
 
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
+# Allows for printing inline for jupyter notebook
+get_ipython().magic('matplotlib inline')
+
+
+# In[2]:
+
 ## Part 1
+DistanceFlagPair = collections.namedtuple('Distance', 'distance flag')
+
 class DataFormat():
 
-=======
-	
-## Part 1
-class DataFormat():
-	
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def func_norm_data(self):
-		src_data = genfromtxt(self.name, delimiter=',')
-		true_data, feature_data = src_data[:,0], np.delete(src_data, 0, axis=1)
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
+    def func_norm_data(self):
+        src_data = genfromtxt(self.name, delimiter=',')
+        flag_data, feature_data = src_data[:,0], np.delete(src_data, 0, axis=1)
+        feature_max, feature_min = np.amax(feature_data, axis=0), np.amin(feature_data, axis=0)
+        feature_range, feature_avg = feature_max - feature_min, np.average(feature_data)
+        norm_data = (feature_data - feature_avg) / feature_range
+        self.norm_data, self.flag_data = norm_data, flag_data
+        return norm_data, flag_data
 
-		feature_max, feature_min = np.amax(feature_data), np.amin(feature_data)
-		feature_range, feature_avg = feature_max - feature_min, np.average(feature_data)
-		feature_norm = (feature_data - feature_avg) / feature_range
-
-		self.norm_data, self.true_data = np.absolute(feature_norm), true_data
-		return np.absolute(feature_norm), true_data
-
-=======
-		
-		feature_max, feature_min = np.amax(feature_data), np.amin(feature_data)
-		feature_range, feature_avg = feature_max - feature_min, np.average(feature_data)
-		feature_norm = (feature_data - feature_avg) / feature_range
-		
-		self.norm_data, self.true_data = np.absolute(feature_norm), true_data
-		return np.absolute(feature_norm), true_data
-		
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
-	def func_leave_out(self, index):
-		return self.norm_data[index], self.true_data[index]
+    def func_leave_out(self, index):
+        return self.norm_data[index], self.flag_data[index]
 
 class KNN():
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
 
-	def __init__(self, norm_data):
-		self.norm_data = norm_data
+    def __init__(self, norm_data, flag_data):
+        self.norm_data, self.flag_data = norm_data, flag_data
 
-	def func_classify(test_data):
+    def func_classify(self, test_data, k):
+        neigbor_pair = self.func_build_neigbor(test_data)
+        flag_sum = np.sum([neigbor.flag for neigbor in neigbor_pair[:k]])
+        return -1 if flag_sum < 0 else 1
+        
+    def func_build_neigbor(self, test_data):
         diff_sqrt = (test_data - self.norm_data) ** 2
         distance_data = np.sqrt(np.sum(diff_sqrt, axis=1))
-        print '+++', distance_data
-=======
-	
-	def __init__(self, norm_data):
-		self.norm_data = norm_data
-		
-	def func_classify():
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
-# Neigbor = collections.namedtuple('Neigbor', 'distance_data true_data')
+        neigbor_pair = []
+        for i in range(len(distance_data)):
+            neigbor = DistanceFlagPair(distance_data[i], self.flag_data[i])
+            neigbor_pair.append(neigbor)
+        return sorted(neigbor_pair, key=operator.attrgetter('distance'))        
 
-# def func_data_class(norm_data, true_data, test_data, k = 1):
-	# diff_sqrt = (test_data - norm_data) ** 2
-	# distance_data = np.sqrt(np.sum(diff_sqrt, axis=1))
-	# neigbor_data = map(Neigbor, distance_data, true_data)
-	# neigbor_data = sorted(neigbor_data, key=operator.attrgetter('distance_data'))
-	# neigbor_data_sum = np.sum([neigbor.true_data for neigbor in neigbor_data[:k]])
-	# return -1 if neigbor_data_sum < 0 else 1
-
-# def func_calc_error(norm_data, true_data, k = 1):
-	# temp_data = map(lambda classifier: func_data_class(norm_data, true_data, classifier, k), norm_data)
-	# return np.sum(np.abs(temp_data - true_data)) / float(2 * len(true_data))
-
-# def func_calc_cross_valid_error(norm_data, true_data, k = 1):
-	# error = 0
-	# for i in range(len(norm_data)):
-		# temp_data = func_data_class(norm_data, true_data, norm_data[i], k)
-		# error += float(np.absolute(true_data[i] - temp_data) / 2)
-	# return float(error / len(norm_data))
-
-if __name__ == '__main__':
-	train_data = DataFormat('knn_train.csv')
-	train_norm_data, true_data = train_data.func_norm_data()
-	# train_leave_out = train_data.func_leave_out(3)
-	# print train_norm_data, train_leave_out
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
-    knn_train = KNN(train_norm_data)
-    knn_train.func_classify(train_norm_data[0])
+def func_calc_error(data, knn, k = 1):
+    temp_data = [knn.func_classify(classifier, k) for classifier in data.norm_data]
+    return np.sum(np.abs(temp_data - data.flag_data)) / float(2 * len(data.flag_data))
     
-=======
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
-	# raw_train, raw_test = func_read_data()
-	# true_data, feature_data = func_filter_data(raw_train)
-	# test_true_data, test_feature_data = func_filter_data(raw_test)
-	# norm_data, test_norm_data = func_norm_data(feature_data), func_norm_data(test_feature_data)
-	# k_list, train_error_list, cross_valid_list, test_error_list = range(1, 52, 2), [], [], []
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
+def func_cross_valid_error(data, knn, k = 1):
+    error = 0
+    for i in range(len(data.norm_data)):
+        norm, flag = data.func_leave_out(i)
+        flag_sum = knn.func_classify(norm, k)
+        flag_error = np.abs(flag - flag_sum) / 2
+        error += flag_error
+    return float(error) / (len(data.flag_data) + 1) 
 
-=======
-	
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
-	# for k in k_list:
-		# train_error = func_calc_error(norm_data, true_data, k)
-		# train_error_list.append(train_error)
-		# cross_valid = func_calc_cross_valid_error(norm_data, true_data, k)
-		# cross_valid_list.append(cross_valid)
-		# test_error = func_calc_error(test_norm_data, test_true_data, k)
-		# test_error_list.append(test_error)
 
-	# print 'Training:', train_error_list
-	# print 'Cross Valid:', cross_valid_list
-<<<<<<< 33e1e6747ad2d09b701afb0ff03fd9fed8e45099
-	# print 'Testing:', test_error_list
-=======
-	# print 'Testing:', test_error_list
->>>>>>> d226101e70227611927aea4dd9d7e82c8c0f7957
+# In[3]:
+
+def func_plot_data(x_list, y_list, label):
+    fig = plt.figure(figsize=(9,6))
+    plt.plot(x_list, y_list, label=label)
+#     plt.legend()
+    fig.suptitle(label + ' Error', fontsize=16)
+    plt.xlabel('K-Value', fontsize=16)
+    plt.ylabel('Error', fontsize=16)
+    
+if __name__ == '__main__':
+    train_data, test_data = DataFormat('knn_train.csv'), DataFormat('knn_test.csv')
+    train_norm_data, train_flag_data = train_data.func_norm_data()
+    test_norm_data, test_flag_data = test_data.func_norm_data()
+    knn_train, knn_test = KNN(train_norm_data, train_flag_data), KNN(test_norm_data, test_flag_data)
+    k_list, train_error_list, cross_valid_list, test_error_list = range(1, 52, 2), [], [], []
+    for k in k_list:
+        train_error = func_calc_error(train_data, knn_train, k)
+        train_error_list.append(train_error)
+        cross_valid = func_cross_valid_error(train_data, knn_train, k)
+        cross_valid_list.append(cross_valid)
+        test_error = func_calc_error(test_data, knn_test, k)
+        test_error_list.append(test_error)
+
+    print 'Training:', train_error_list
+    func_plot_data(k_list, train_error_list, 'Training Data')
+    
+    print 'Cross Valid:', cross_valid_list
+    func_plot_data(k_list, cross_valid_list, 'Cross Validation')
+    
+    print 'Testing:', test_error_list    
+    func_plot_data(k_list, test_error_list, 'Testing Data')
+
+
+# In[ ]:
+
+
+
