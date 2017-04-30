e160e3f95de0b55a564c12588f2551f8e4db923a
MrFengShui <luansongjian@outlook.com>
2017-04-29 17:00:31 -0700

    add dt code

--- HW3/Code/Assignment_Three_Final.py
+++ HW3/Code/Assignment_Three_Final.py
@@ -13,7 +13,15 @@ class DataFormat():
     def __init__(self, name):
         self.name = name
 
-    def func_norm_data(self):
+    def __init__(self, name = None, norm_data = None, flag_data = None):
+        self.name = name
+        self.norm_data = np.array(norm_data)
+        self.flag_data = np.array(flag_data)
+
+    def data_set(self):
+        return genfromtxt(self.name, delimiter=',')
+
+    def norm_flag_data(self):
         src_data = genfromtxt(self.name, delimiter=',')
         flag_data, feature_data = src_data[:,0], np.delete(src_data, 0, axis=1)
         feature_max, feature_min = np.amax(feature_data, axis=0), np.amin(feature_data, axis=0)
@@ -22,7 +30,7 @@ class DataFormat():
         self.norm_data, self.flag_data = norm_data, flag_data
         return norm_data, flag_data
 
-    def func_leave_out(self, index):
+    def leave_out(self, index):
         return self.norm_data[index], self.flag_data[index]
 
 class KNN():
@@ -51,7 +59,7 @@ def func_calc_error(data, knn, k = 1):
 def func_cross_valid_error(data, knn, k = 1):
     error = 0
     for i in range(len(data.norm_data)):
-        norm, flag = data.func_leave_out(i)
+        norm, flag = data.leave_out(i)
         flag_sum = knn.func_classify(norm, k)
         flag_error = np.abs(flag - flag_sum) / 2
         error += flag_error
@@ -80,9 +88,9 @@ class DecisionStump():
         row_count, col_count = self.norm_data.shape
         upper_pos_count, upper_neg_count, lower_pos_count, lower_neg_count = self.count_one(index, value)
         upper_entropy = func_calc_entropy(upper_pos_count, upper_neg_count)
-        upper_entropy = float(upper_pos_count + upper_neg_count) / row_count * upper_entropy
+        upper_entropy = float(upper_pos_count + upper_neg_count) * upper_entropy / row_count
         lower_entropy = func_calc_entropy(lower_pos_count, lower_neg_count)
-        lower_entropy = float(lower_pos_count + upper_neg_count) / row_count * lower_entropy
+        lower_entropy = float(lower_pos_count + lower_neg_count) * lower_entropy / row_count
         return init_entropy - upper_entropy - lower_entropy
 
     def count_one(self, index, value):
@@ -107,10 +115,98 @@ class DecisionStump():
             else: false += 1
         return float(true) / (true + false)
 
+class TreeNode():
+
+    def __init__(self, column = -1, value = None, rs = None, left = None, right = None):
+        self.column, self.value = column, value
+        self.rs, self.left, self.right = rs, left, righ
+
 class DecisionTree():
 
-    def __init__(self):
-        print None
+    def __init__(self, norm_data, flag_data, depth, height):
+        self.norm_data, self.flag_data = norm_data, flag_data
+        self.depth, self.height = depth, height
+        self.cur_info_gain, self.feature_num, self.split_num = 0, 0, 0
+        if depth > 0:
+            self.cur_info_gain, self.feature_num, self.split_num = self.format_data()
+            if self.cur_info_gain > 0:
+                data_left, data_right = self.split_data(self.norm_data, self.flag_data, self.feature_num, self.split_num)
+                #LHS
+                left_norm_data, left_flag_data = data_left.norm_data, data_left.flag_data
+                self.left_child = DecisionTree(left_norm_data, left_flag_data, self.depth - 1, self.height + 1)
+                #RHS
+                right_norm_data, right_flag_data = data_right.norm_data, data_right.flag_data
+                self.right_child = DecisionTree(right_norm_data, right_flag_data, self.depth - 1, self.height + 1)
+            else:
+                self.depth = 0
+
+    def format_data(self):
+        row, col = self.norm_data.shape
+        cur_info_gain, feature_num, split_num = 0, 0, 0
+        for index in range(col):
+            for value in self.norm_data.T[index]:
+                tmp_info_gain = self.info_gain(index, value)
+                if cur_info_gain < tmp_info_gain:
+                    cur_info_gain = tmp_info_gain
+                    feature_num, split_num = index, value
+        return cur_info_gain, feature_num, split_num
+
+    def split_data(self, norm_data, flag_data, feature_num, split_num):
+        row, col = norm_data.shape
+        left_norm, left_flag = [], []
+        right_norm, right_flag = [], []
+        for i in range(col):
+            item_data = norm_data[i]
+            if item_data[i] > split_num:
+                right_norm.append(item_data[i])
+                right_flag.append(flag_data[i])
+            else:
+                left_norm.append(item_data[i])
+                left_flag.append(flag_data[i])
+        return DataFormat(norm_data = left_norm, flag_data = left_flag), DataFormat(norm_data = right_norm, flag_data = right_flag)
+
+    def info_gain(self, index, value):
+        pos, neg = func_count_one(self.flag_data)
+        init_entropy = func_calc_entropy(pos, neg)
+        row_count, col_count = self.norm_data.shape
+        upper_pos_count, upper_neg_count, lower_pos_count, lower_neg_count = self.count_one(index, value)
+        upper_entropy = func_calc_entropy(upper_pos_count, upper_neg_count)
+        upper_entropy = float(upper_pos_count + upper_neg_count) * upper_entropy / row_count
+        lower_entropy = func_calc_entropy(lower_pos_count, lower_neg_count)
+        lower_entropy = float(lower_pos_count + lower_neg_count) * lower_entropy / row_count
+        return init_entropy - upper_entropy - lower_entropy
+
+    def count_one(self, index, value):
+        upper_pos_count, upper_neg_count = 0, 0
+        lower_pos_count, lower_neg_count = 0, 0
+        norm_data_t = self.norm_data.T[index]
+        for i in range(len(norm_data_t)):
+            tmp_value = norm_data_t[i]
+            if tmp_value > value:
+                if self.flag_data[i] > 0: upper_pos_count += 1
+                else: upper_neg_count += 1
+            else:
+                if self.flag_data[i] > 0: lower_pos_count += 1
+                else: lower_neg_count += 1
+        return upper_pos_count, upper_neg_count, lower_pos_count, lower_neg_count
+
+    def calc_error_rate(self, feature_num, split_num):
+        predict, true, false = 0, 0, 0
+        for i in range(len(self.norm_data)):
+            predict = -1 if self.norm_data[i][feature_num] < split_num else 1
+            if predict == self.flag_data[i]: true += 1
+            else: false += 1
+        return float(true) / (true + false)
+
+    def print_tree(self):
+        if self.depth > 0:
+            print '+' * self.height, self.feature_num, self.split_num
+            self.left_child.print_tree()
+            print '+' * self.height, self.feature_num, self.split_num
+            self.right_child.print_tree()
+            print '+' * self.height, self.cur_info_gain
+        else:
+            print None
 
 def func_count_one(flag_data):
     pos, neg = 0, 0
@@ -130,8 +226,8 @@ def func_calc_entropy(pos, neg):
 ## Main Part
 def func_part_one():
     train_data, test_data = DataFormat('knn_train.csv'), DataFormat('knn_test.csv')
-    train_norm_data, train_flag_data = train_data.func_norm_data()
-    test_norm_data, test_flag_data = test_data.func_norm_data()
+    train_norm_data, train_flag_data = train_data.norm_flag_data()
+    test_norm_data, test_flag_data = test_data.norm_flag_data()
     knn_train, knn_test = KNN(train_norm_data, train_flag_data), KNN(test_norm_data, test_flag_data)
     k_list, train_error_list, cross_valid_list, test_error_list = range(1, 52, 2), [], [], []
     for k in k_list:
@@ -148,25 +244,26 @@ def func_part_one():
 
 def func_part_two():
     train_data, test_data = DataFormat('knn_train.csv'), DataFormat('knn_test.csv')
-    train_norm_data, train_flag_data = train_data.func_norm_data()
-    test_norm_data, test_flag_data = test_data.func_norm_data()
+    train_norm_data, train_flag_data = train_data.norm_flag_data()
+    test_norm_data, test_flag_data = test_data.norm_flag_data()
     ''' Decision Stump '''
-    train_stump, test_stump = DecisionStump(train_norm_data, train_flag_data), DecisionStump(test_norm_data, test_flag_data)
-    train_info_gain, train_feature, train_split = train_stump.build_stump()
-    test_info_gain, test_feature, test_split = test_stump.build_stump()
-    # Training Data Outputs
-    print 'Training Information Gain Value:', train_info_gain
-    print 'Training Feature Value:', train_feature
-    # print 'Training Splitted Value:', train_split
-    print 'Training Error Rate:', train_stump.calc_error_rate(train_feature, train_split)
-    print
-    # Testing Data outputs
-    print 'Testing Information Gain Value:', test_info_gain
-    print 'Testing Feature Value:', test_feature
-    # print 'Testing Splitted Value:', test_split
-    print 'Testing Error Rate:', test_stump.calc_error_rate(test_feature, test_split)
+    # train_stump, test_stump = DecisionStump(train_norm_data, train_flag_data), DecisionStump(test_norm_data, test_flag_data)
+    # train_info_gain, train_feature, train_split = train_stump.build_stump()
+    # test_info_gain, test_feature, test_split = test_stump.build_stump()
+    # # Training Data Outputs
+    # print 'Training Information Gain Value:', train_info_gain
+    # print 'Training Feature Value:', train_feature
+    # # print 'Training Splitted Value:', train_split
+    # print 'Training Error Rate:', train_stump.calc_error_rate(train_feature, train_split)
+    # print
+    # # Testing Data outputs
+    # print 'Testing Information Gain Value:', test_info_gain
+    # print 'Testing Feature Value:', test_feature
+    # # print 'Testing Splitted Value:', test_split
+    # print 'Testing Error Rate:', test_stump.calc_error_rate(test_feature, test_split)
     ''' Decision Tree '''
-    train_data = DecisionTree()
+    train_tree = DecisionTree(train_norm_data, train_flag_data, 5, 0)
+    train_tree.print_tree()
 
 if __name__ == '__main__':
     # func_part_one()
---
+++ HW3/Code/Sample_Code.py
@@ -0,0 +1,264 @@
+import math
+from math import log
+import numpy as np
+from collections import namedtuple
+from operator import attrgetter
+
+DistanceOutputPair = namedtuple('DistanceOutputPair', 'distance output')
+FeatureOutputPair = namedtuple('FeatureOutputPair', 'features output')
+
+class CsvPrinter:
+
+    def __init__(self, filename, labels=[], delimiter=','):
+        if labels:
+            self.file = open(filename, 'w')
+            self.columns = len(labels)
+            labels.append('\n')
+            self.delimiter = delimiter
+            self.file.write(self.delimiter.join(labels))
+        else:
+            print "CsvPrinter: Please provide column labels"
+            exit()
+
+    def writerow(self, data):
+        if len(data) == self.columns:
+            data.append('\n')
+            self.file.write(self.delimiter.join([str(x) for x in data]))
+        else:
+            print "CsvPrinter: Data length should match number of labels"
+            exit()
+
+    def close(self):
+        self.file.close()
+
+class Data:
+    def __init__(self, filename=None, features=None, outputs=None):
+        if filename:
+            file = open(filename)
+            (self.features, self.outputs) = self._build_data_arrays(file)
+            file.close()
+        elif features and outputs:
+            self.features = features
+            self.outputs = outputs
+        self._features = self.features
+        self._outputs = self.outputs
+
+    def normalize(self):
+        features_max = np.amax(self.features, axis=0)
+        features_min = np.amin(self.features, axis=0)
+        features_spread = features_max - features_min
+        self.features = (self.features - features_min) / features_spread
+        self._features = self.features
+
+    def leave_out(self, index):
+        self.features = np.delete(self._features, index, axis=0)
+        self.outputs = np.delete(self._outputs, index, axis=0)
+        return FeatureOutputPair(self._features[index], self._outputs[index])
+
+    def include_all(self):
+        self.features = self._features
+        self.outputs = self._outputs
+
+    def _build_data_arrays(self, file):
+        (features, outputs) = self._build_data_lists(file)
+        return (np.array(features, dtype=float), np.array(outputs, dtype=int))
+
+    def _build_data_lists(self, file):
+        (features, outputs) = ([], [])
+        for line in file:
+            (line_features, line_output) = self._extract_features_and_output(line)
+            features.append(line_features)
+            outputs.append(line_output)
+        return (features, outputs)
+
+    def _extract_features_and_output(self, line):
+        features_and_output = line.split(',')
+        return (features_and_output[1:], features_and_output[0])
+
+class Knn:
+    def __init__(self, training_data):
+        self.training_data = training_data
+
+    def get_testing_error(self, data, k=1):
+        outputs = self._test(data, k)
+        return (sum(abs(outputs - data.outputs)) / float(2 * len(data.outputs)))
+
+    def get_training_error(self, k=1):
+        outputs = self._test(self.training_data, k)
+        return (sum(abs(outputs - self.training_data.outputs)) /
+                float(2 * len(self.training_data.outputs)))
+
+    # Specifically for leave-one-out cross-validation
+    def get_cross_validation_error(self, k=1):
+        total_error = 0
+        for i in range(len(self.training_data.features)):
+            left_out = self.training_data.leave_out(i)
+            output = self._classify(left_out.features, k)
+            total_error += abs(left_out.output - output)/2
+        self.training_data.include_all()
+        return float(total_error) / len(self.training_data.outputs)
+
+    def _test(self, data, k):
+        return map(lambda instance: self._classify(instance, k), data.features)
+
+    def _classify(self, test_instance, k):
+        neighbors = self._get_nearest_neighbors(test_instance)
+        output = sum([neigbor.output for neigbor in neighbors[:k]])
+        if output < 0: return -1
+        else: return 1
+
+    def _get_nearest_neighbors(self, test_instance):
+        distances = np.sqrt(np.sum(
+                (test_instance - self.training_data.features)**2,
+                axis=1
+                ))
+
+        neigbors = map(DistanceOutputPair, distances,
+            self.training_data.outputs)
+
+        return sorted(neigbors, key=attrgetter('distance'))
+
+class Stump:
+
+    def __init__(self, training_data):
+        self.training_data = training_data
+        self.trained_information_gain = None
+        self.trained_feature = None
+        self.trained_split = None
+
+    def train_stump(self):
+        data_points, feature_number = self.training_data.features.shape
+        # print '+++', data_points, feature_number
+        self.trained_information_gain = 0
+        self.trained_feature = 0
+        self.trained_split = 0
+        #increment through each feature
+        for i in range(0,feature_number):
+            #test information gain using data point as a boundary
+            for j in self.training_data.features.T[i]:
+                boundary_value = j
+                cur_information_gain = self._information_gain(i, j)
+                if self.trained_information_gain < cur_information_gain:
+                    self.trained_information_gain = cur_information_gain
+                    self.trained_feature= i
+                    self.trained_split = j
+
+    def _information_gain(self, feature, boundary_value):
+        initial_uncertainty = self._initial_uncertainty()
+        data_points, feature_number = self.training_data.features.shape
+        greater_than_split_pos = 0
+        greater_than_split_neg = 0
+        less_than_split_pos = 0
+        less_than_split_neg = 0
+        # print '+++', len(self.training_data.features.T[feature]), feature, boundary_value
+        for j, value in enumerate(self.training_data.features.T[feature]):
+            if value > boundary_value:
+                if self.training_data.outputs[j] > 0:
+                    greater_than_split_pos += 1
+                else:
+                    greater_than_split_neg += 1
+            else:
+                if self.training_data.outputs[j] > 0:
+                    less_than_split_pos += 1
+                else:
+                    less_than_split_neg += 1
+        # print '+++', greater_than_split_pos, greater_than_split_neg
+        greater_entropy = self._entropy(greater_than_split_pos, greater_than_split_neg)
+        greater_p = ((float)(greater_than_split_pos+greater_than_split_neg)/data_points)*greater_entropy
+
+        less_entropy = self._entropy(less_than_split_pos, less_than_split_neg)
+        less_p = ((float)(less_than_split_pos+less_than_split_neg)/data_points)*less_entropy
+
+        gain = initial_uncertainty - greater_p - less_p
+
+        return gain
+
+    def _entropy(self, pos, neg):
+        sum = pos+neg
+        if pos == 0 or neg == 0:
+            return 0
+        else:
+            return -(((float)(pos)/sum)*log(((float)(pos)/sum),2)) - (((float)(neg)/sum)*log(((float)(neg)/sum),2))
+
+    def _initial_uncertainty(self):
+        pos = 0
+        neg = 0
+        for i in self.training_data.outputs:
+            if i > 0:
+                pos += 1
+            else:
+                neg += 1
+        return self._entropy(pos, neg)
+
+    def accuracy(self, data):
+        prediction = 0
+        correct = 0
+        wrong = 0
+        for i, x in enumerate(data.features):
+            if x[self.trained_feature] > self.trained_split:
+                prediction = 1
+            else:
+                prediction = -1
+
+            if prediction == data.outputs[i]:
+                correct += 1
+            else:
+                wrong += 1
+
+        return float(correct)/(correct+wrong)
+
+
+
+training_data = Data('knn_train.csv')
+testing_data = Data('knn_test.csv')
+training_data.normalize()
+testing_data.normalize()
+
+def main():
+    #part_1()
+    part_2()
+    extra_credit()
+
+def part_1():
+    labels=['k', 'Training Error', 'Testing Error', 'Leave-one-out Error']
+    csv = CsvPrinter('reports/part_1.csv', labels)
+    model = Knn(training_data)
+
+    def _print_results():
+        print "---------------------------"
+        print "K-value: ", k
+        print "".join(["Training Error: ",
+            str(round(training_err, 4) * 100), "%"]
+            )
+        print "".join(["Leave-One-Out Error: ",
+            str(round(cv_err, 4) * 100), "%"]
+            )
+        print "".join(["Testing Error: ",
+            str(round(testing_err, 4) * 100), "%"]
+            )
+
+    for k in range(1, 52, 2):
+        training_err = model.get_training_error(k=k)
+        testing_err = model.get_testing_error(testing_data, k=k)
+        cv_err = model.get_cross_validation_error(k=k)
+        _print_results()
+        csv.writerow([k, training_err, testing_err, cv_err])
+    print "---------------------------"
+    csv.close()
+
+def part_2():
+    model = Stump(training_data)
+    model.train_stump()
+    print "----------Part 2-----------"
+    print "Split if Value is > " + str(model.trained_split) + " On feature " + str(model.trained_feature)
+    print "Information Gain of " + str(model.trained_information_gain)
+
+    print "Training Accuracy: " + str(model.accuracy(training_data))
+    print "Testing Accuracy: " + str(model.accuracy(testing_data))
+    print "---------------------------"
+    return
+
+def extra_credit():
+    return
+
+if __name__ == '__main__':main()
